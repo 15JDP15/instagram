fbe79984582b8a02d15c77091e2998a876d344e6
15JDP15 <156719053+15JDP15@users.noreply.github.com>
2024-02-16 16:23:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.login-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevenir el envío del formulario
+    alert('Inicio de sesión no implementado en este demo.');
+});
